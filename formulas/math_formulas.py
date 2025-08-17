e405f7946d940b2295450630d64bd9fa748831ac
vijaijvk <40302989+vijaijvk@users.noreply.github.com>
2025-08-16 23:54:23 -0700

    Create math_formulas.py

---
+++ formulas/math_formulas.py
@@ -0,0 +1,14 @@
+math_formulas = {
+    "Grade 5": {
+        "Arithmetic": {
+            "Area of Rectangle": "Area = Length × Width",
+            "Perimeter": "Perimeter = 2 × (Length + Width)"
+        }
+    },
+    "Grade 8": {
+        "Algebra": {
+            "Slope": "Slope = (y₂ - y₁) ÷ (x₂ - x₁)",
+            "Quadratic Formula": "x = [-b ± √(b² - 4ac)] ÷ 2a"
+        }
+    }
+}
