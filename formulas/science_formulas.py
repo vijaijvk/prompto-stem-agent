59c4785c6271b4984a5eb591f8e7aaa0aed451fd
vijaijvk <40302989+vijaijvk@users.noreply.github.com>
2025-08-16 23:55:26 -0700

    Create science_formulas.py

---
+++ formulas/science_formulas.py
@@ -0,0 +1,22 @@
+science_formulas = {
+    "Grade 6": {
+        "Physics": {
+            "Speed": "Speed = Distance ÷ Time",
+            "Force": "Force = Mass × Acceleration"
+        },
+        "Chemistry": {
+            "Density": "Density = Mass ÷ Volume",
+            "Water Formula": "H₂O"
+        }
+    },
+    "Grade 9": {
+        "Physics": {
+            "Ohm’s Law": "V = I × R",
+            "Power": "Power = Work ÷ Time"
+        },
+        "Chemistry": {
+            "Mole Concept": "n = m ÷ M",
+            "pH": "pH = –log[H⁺]"
+        }
+    }
+}
